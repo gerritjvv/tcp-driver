2e05b986dfda41c52ad3cf1ee293e792e0bb8574
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2016-02-01 16:59:27 +0100

    first release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tcp-driver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tcp-driver "0.1.0"
+  :description "Java/Clojure TCP Connections done right"
+  :url "https://github.com/gerritjvv/tcp-driver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
