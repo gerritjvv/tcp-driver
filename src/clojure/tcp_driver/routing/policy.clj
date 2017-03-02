0aac478eca8289670ea085f86d17246f961ab1ce
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2017-03-03 00:29:53 +0200

    update pool key select

--- src/clojure/tcp_driver/routing/policy.clj
+++ src/clojure/tcp_driver/routing/policy.clj
@@ -42,20 +42,20 @@
 
            (-add-host! [_ host]
                        (ensure-host-address-schema! host)
-                       (swap! hosts-at conj host))
+                       (swap! hosts-at conj (select-keys host [:host :port])))
 
            (-remove-host! [_ host]
                           (ensure-host-address-schema! host)
-                          (swap! hosts-at disj host))
+                          (swap! hosts-at disj (select-keys host [:host :port])))
 
            (-blacklisted? [_ host]
                           (ensure-host-address-schema! host)
-                          (get black-listed-hosts-cache host))
+                          (get black-listed-hosts-cache (select-keys host [:host :port])))
 
            (-blacklist! [_ host]
                         (ensure-host-address-schema! host)
                         (prn "DefaultRountingPolicy:-blacklist " host " in " black-listed-hosts-cache)
-                        (assoc black-listed-hosts-cache host true))
+                        (assoc black-listed-hosts-cache (select-keys host [:host :port]) true))
 
            (-select-host [_]
                          (let [available-hosts (into [] (clj-set/difference @hosts-at (set (keys black-listed-hosts-cache))))]
